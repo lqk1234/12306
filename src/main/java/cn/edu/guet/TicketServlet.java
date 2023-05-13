9bf66d94b379fdb0ca673043dac5f70c3bf110a4
lqk <3190848413@qq.com>
2023-05-13 13:44:01 +0800

    1,Ajax发送请求获取车次数据 2,美化JSON的输出格式

--- src/main/java/cn/edu/guet/TicketServlet.java
+++ src/main/java/cn/edu/guet/TicketServlet.java
@@ -1,26 +1,34 @@
 package cn.edu.guet;
 
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.io.Serializable;
 
 public class TicketServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("余票查询");
-        String startStation = request.getParameter("startStation");
-        System.out.println("起始站: " + startStation);
-        String endStation = request.getParameter("endStation");
-        System.out.println("终点站: " + endStation);
-        String departureDate = request.getParameter("departureDate");
-        System.out.println("到站日期: " + departureDate);
-        String allTickets = TicketSearch.search(startStation,endStation,departureDate);
-        //设置响应类型
+
+        String fromStation = request.getParameter("fromStation");
+        System.out.println("起始站：" + fromStation);
+        String toStation = request.getParameter("toStation");
+        System.out.println("终点站："+toStation);
+
+        String departureTime=request.getParameter("departureTime");
+        System.out.println("出发日期："+departureTime);
+
+        String ticketList = TicketSearch.search(fromStation, toStation, departureTime);
+
+        String json=JSON.toJSONString(ticketList, SerializerFeature.PrettyFormat);
+
         response.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.print(allTickets);
+        PrintWriter out=response.getWriter();
+        out.print(json);
         out.flush();
         out.close();
     }
